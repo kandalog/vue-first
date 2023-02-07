7e0feaae518f5ec59c109553b90acd78f32ea991
kandalog <asuasu.tee@gmail.com>
2023-02-07 19:43:18 +0900

    削除機能を実装

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 const app = Vue.createApp({
   data: () => ({
     newItem: "",
-    todos: [],
+    todos: [
+      {
+        item: "task1",
+        isDone: false,
+      },
+    ],
   }),
   methods: {
+    // 新規タスク追加
     addItem: function (e) {
-      console.log("clicked");
+      if (this.newItem === "") return;
+
       let todo = {
         item: this.newItem,
+        isDone: false,
       };
+
       this.todos.push(todo);
+
       this.newItem = "";
+    },
+
+    // タスクを削除
+    deleteItem: function (index) {
+      console.log("delete");
+      this.todos.splice(index, 1);
     },
   },
 });
